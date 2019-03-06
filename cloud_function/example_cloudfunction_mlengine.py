4b2ef5fc616e11eec30ebd4855a3cf823a32d831
qoala <fatchur.rahman1@gmail.com>
2019-03-06 05:24:04 -0500

    u

--- cloud_function/example_cloudfunction_mlengine.py
+++ cloud_function/example_cloudfunction_mlengine.py
@@ -5,63 +5,63 @@ import json
 import base64
 import cv2
 import numpy as np
+from PIL import Image
+import io
+
 
 classes = ['class1', 'class2', 'class3']
 
+
+# Take in base64 string and return PIL image
+def stringToImage(base64_string):
+    imgdata = base64.b64decode(base64_string)
+    return Image.open(io.BytesIO(imgdata))
+
+
 def predict(json_data):
-	PROJECTID = 'your project id NOT your project name'
-	projectID = 'projects/{}'.format(PROJECTID)
-	modelName = 'your model name'
-	modelID = '{}/models/{}'.format(projectID, modelName)
-	credentials = GoogleCredentials.get_application_default()
-	ml = discovery.build('ml', 'v1', credentials=credentials)
-	request_body = {"instances": [json_data]}
-	req = ml.projects().predict(name=modelID, body=request_body)
-	#resp = req.execute()
-	resp = None
-	status = 'fail'
+    PROJECTID = 'your project id NOT your project name'
+    projectID = 'projects/{}'.format(PROJECTID)
+    modelName = 'your model name'
+    modelID = '{}/models/{}'.format(projectID, modelName)
+    credentials = GoogleCredentials.get_application_default()
+    ml = discovery.build('ml', 'v1', credentials=credentials)
+    request_body = {"instances": [json_data]}
+    req = ml.projects().predict(name=modelID, body=request_body)
+
+    resp = None
+    status = 'fail'
    
-	try:
-		resp = req.execute()
-		status = 'success'
-	except errors.HttpError as err:
-		resp = str(err._get_reason())
-	
-	return resp, status
+    try:
+        resp = req.execute()
+        status = 'success'
+    except errors.HttpError as err:
+        resp = str(err._get_reason())
+    
+    return resp, status
 
 
 
 def mykad_predictor(request):
-	"""Responds to any HTTP request.
-	Args:
-		request (flask.Request): HTTP request object.
-	Returns:
-		The response text or any set of values that can be turned into a
-		Response object using
-		`make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-	"""
-	request_json = request.get_json()
-	file = request.files['image']
-	img = file.read()
-	
-	img = cv2.imdecode(np.fromstring(img, dtype=np.uint8), -1)
-	img = cv2.resize(img, (100, 100))
-	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-	img = img.astype(np.float32)
-	img = img/255.
-	img = img.tolist()
-	json_data = json.dumps(img)
-	
-	res, status = predict(json.loads(json_data))
-	class_prediction = None
-	prediction_dict = {}
-	
-	if status == 'success':
-		res_prediction = np.argmax(res['predictions'][0]["output"])
-		class_prediction = classes[res_prediction]
-	else:
-		class_prediction = 'failed_to_recognize'
-	
-	prediction_dict['result'] = class_prediction
-		
-	return (str(prediction_dict))
+    request_json = request.get_json()
+    base64string = request_json.get('image', None)
+    img = np.array(stringToImage(base64string))
+    
+    img = cv2.resize(img, (100, 100))
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.astype(np.float32)
+    img = img/255.
+    img = img.tolist()
+    json_data = json.dumps(img)
+
+    res, status = predict(json.loads(json_data))
+    class_prediction = None
+    prediction_dict = {}
+    
+    if status == 'success':
+        res_prediction = np.argmax(res['predictions'][0]["output"])
+        class_prediction = classes[res_prediction]
+    else:
+        class_prediction = 'failed_to_recognize'
+    
+    prediction_dict['result'] = class_prediction
+    return (str(prediction_dict))
