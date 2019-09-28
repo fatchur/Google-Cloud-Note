2f1fabfd121fce5cf81e875a52d753ab7dd1f23b
qoala <fatchur.rahman1@gmail.com>
2019-09-28 04:41:02 -0400

    UPDATE storage upload url

--- storage/url_upload/example.py
+++ storage/url_upload/example.py
@@ -7,10 +7,15 @@ app = Flask(__name__)
 
 @app.route("/get_url", methods=['POST', 'PUT'])
 def upload_file(req):
+    filename = str(uuid.uuid4())
+    time = str(datetime.datetime.now())
+    time = time.replace(" ", "-")
+    
     BUCKET = 'bucket name'
     gcs = storage.Client()
     bucket = gcs.get_bucket(BUCKET)
-    blob = bucket.blob('the filename for your gcs')
+    
+    blob = bucket.blob(filename + "--" + time + ".mp4")
     signed_url = blob.create_resumable_upload_session(content_type='video/webm')
         
     return signed_url
