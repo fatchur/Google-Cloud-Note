7fb8475ee6e30a1c5aa7bd403a7196ffdb5cacc6
root <fatchur.rahman1@gmail.com>
2020-08-05 00:31:23 +0700

    [ADD] example obj detection

---
+++ automl/object_detection_api.py
@@ -0,0 +1,25 @@
+def localize_objects(path):
+    """Localize objects in the local image.
+
+    Args:
+    path: The path to the local file.
+    """
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, 'rb') as image_file:
+        content = image_file.read()
+    image = vision.types.Image(content=content)
+
+    objects = client.object_localization(
+        image=image).localized_object_annotations
+
+    print('Number of objects found: {}'.format(len(objects)))
+    for object_ in objects:
+        print('\n{} (confidence: {})'.format(object_.name, object_.score))
+        print('Normalized bounding polygon vertices: ')
+        for vertex in object_.bounding_poly.normalized_vertices:
+            print(' - ({}, {})'.format(vertex.x, vertex.y))
+
+
+localize_objects('images/phone3.jpg')
